ed090c6cf76ebe14bd01ea3d46042206172935e1
山南 <zhangpeichaook@163.com>
2022-04-29 16:49:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import { shuffle, uuid, appendFunction, prependFunction } from './utils.js';
+import { appendFunction, prependFunction, shuffle, uuid } from './utils.js';
+
+import { version } from '../package.json';
 
 let lynx = {
+    version: version,
     shuffle,
     uuid,
     appendFunction,
     prependFunction
 };
+
+console.info('You are using lynx v' + lynx.version);
 
 export default lynx;
